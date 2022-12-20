9f84a18a8dbfc844a9cf5497766220a169028a30
Lambourl <lmbrluc@gmail.com>
2022-12-20 16:40:39 +0100

    :wink:

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"hello world "<<std::endl;
+    /*
+    * TO DO : Make something
+    */
+
+    /*
+    # TO DO : Make something2 
+    */
+
+
+
+    return 0;
+}
