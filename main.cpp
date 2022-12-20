9d7f656a955fb9379a7d578d370289a0ab1d1b22
Lambourl <lmbrluc@gmail.com>
2022-12-20 16:46:17 +0100

    :wrench:

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main(){
+
+    /*
+        # TODO Come up with a more imaginative greeting (title)
+        #  Everyone uses hello world and it's boring. (Corps)
+        ## other line
+        other other line
+        #  labels: help wanted
+        #  assignees: Lambourl, bouteillerAlan, hbjydev
+        #  milestone: 1
+
+*/
     std::cout<<"hello world "<<std::endl;
 
 /*
