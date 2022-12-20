2d6de53f9847633806d6f06251bf4749b01ef27e
Lambourl <lmbrluc@gmail.com>
2022-12-20 16:45:09 +0100

    :wink:

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ int main(){
     std::cout<<"hello world "<<std::endl;
 
 /*
-  # TODO Come up with a more imaginative greeting
-        #  Everyone uses hello world and it's boring.
+        # TODO Come up with a more imaginative greeting (title)
+        #  Everyone uses hello world and it's boring. (Corps)
+        ## other line
+        other other line
         #  labels: help wanted
         #  assignees: Lambourl, bouteillerAlan, hbjydev
         #  milestone: 1
